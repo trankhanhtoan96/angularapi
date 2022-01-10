e5189c12e51b02cc560434ccf8525ba38f518828
tkt <trankhanhtoan96@gmail.com>
2022-01-11 03:18:57 +0700

    home

--- src/app/globalcomponents/components/FEB3.ts
+++ src/app/globalcomponents/components/FEB3.ts
@@ -12,7 +12,7 @@ import {Component} from '@angular/core';
                     </a>
                 </h2>
                 <p class="style-description mb-0" style="color: #f1f1f1">Tối qua, Facebook đã thông báo đổi tên công ty thành Meta, nhằm phản ánh tham vọng tăng trưởng vượt ngoài mạng xã hội của công ty. Quyết định đổi tên này cho…</p>
-                <p class="style-name-author mt-1" style="font-size: 13px;color:#f1f1f1;font-weight: normal">
+                <p class="style-name-author mt-2" style="font-size: 13px;color:#f1f1f1;font-weight: normal">
                     <i class="ti ti-user-check"></i><a style="color:#f1f1f1" href="/writer/6">Thành Vương Tiến</a> · <i class="ti ti-calendar-event"></i> 08, tháng 12, 2021
                 </p>
             </div>
