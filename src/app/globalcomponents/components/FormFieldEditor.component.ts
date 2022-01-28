b75d9a903e6fbc80ddfe1a1e1e1b598545330e53
tkt <trankhanhtoan96@gmail.com>
2022-01-28 16:29:32 +0700

    paste from word

--- src/app/globalcomponents/components/FormFieldEditor.component.ts
+++ src/app/globalcomponents/components/FormFieldEditor.component.ts
@@ -17,8 +17,8 @@ export class FormFieldEditorComponent implements OnInit {
             base_url: '/tinymce',
             suffix: '.min',
             height: 400,
-            plugins: ['fullscreen link image code media table lists paste image paste'],
-            toolbar: 'fullscreen formatselect paste | bold italic | alignleft aligncenter alignright | bullist numlist | image',
+            plugins: ['fullscreen link image code media table lists paste image'],
+            toolbar: 'fullscreen formatselect | bold italic | alignleft aligncenter alignright | bullist numlist | image',
             images_upload_handler: this.imageUploadHandler,
             paste_data_images: true
         }
