c2c41d1a1a685ff5f106c79d70e937557e663a30
tkt <trankhanhtoan96@gmail.com>
2022-01-29 09:53:39 +0700

    report

--- src/app/modules/administration/components/Administration.component.ts
+++ src/app/modules/administration/components/Administration.component.ts
@@ -25,7 +25,7 @@ export class AdministrationComponent implements OnInit {
     public selectedTopic = '';
     public intervalOptions = listInterval;
     public topicOptions = [{id: '', name: 'Tất cả'}];
-    startdate = new Date(new Date().getFullYear()+'-'+new Date().getMonth()+'-01');
+    startdate = new Date(new Date().getFullYear().toString()+'-'+(new Date().getMonth()+1).toString()+'-01');
     enddate = new Date();
 
     constructor(private backend: Backend, private model: Model) {
